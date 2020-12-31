680d9eb03da4bc02eb5218fa314c032472e3f3fc
wjx163 <78280484@qq.com>
2020-12-31 15:48:08 +0800

    'add'

---
+++ email.go
@@ -0,0 +1,15 @@
+package email
+
+import "fmt"
+
+func Email()  {
+	fmt.Println("mail func test")
+}
+
+func Test()  {
+	fmt.Println("test func")
+}
+
+func Index()  {
+	fmt.Println("index func")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wjx163/go-email
+
+go 1.15
